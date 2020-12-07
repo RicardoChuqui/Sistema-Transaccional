9b6b93cc080a18817229a8626b1d1bdbe92255c2
ricardo <rchuquio@est.ups.edu.ec>
2020-12-06 19:04:18 -0500

    modelo

---
+++ Sistema-Transaccional/src/main/java/ec/ups/edu/appdis/g1/modelo/Rol.java
@@ -0,0 +1,54 @@
+package ec.ups.edu.appdis.g1.modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Rol implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_rol")
+	private int idrol;
+	
+	@Column(name = "nombre_rol")
+	private String nombre;
+	
+	@OneToMany(mappedBy = "rol",fetch = FetchType.EAGER,cascade = {CascadeType.ALL})
+	private List<UsuarioAdministrativo> usuario;
+
+	public int getIdrol() {
+		return idrol;
+	}
+
+	public void setIdrol(int idrol) {
+		this.idrol = idrol;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<UsuarioAdministrativo> getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(List<UsuarioAdministrativo> usuario) {
+		this.usuario = usuario;
+	}
+		
+}
