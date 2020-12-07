46854bbfea35a2fad588a1cfba0b9cfe802c4b9c
ricardo <rchuquio@est.ups.edu.ec>
2020-12-07 14:20:29 -0500

    rol

---
+++ Sistema-Transaccional/src/main/java/ec/ups/edu/appdis/g1/negocio/RolON.java
@@ -0,0 +1,47 @@
+package ec.ups.edu.appdis.g1.negocio;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ec.ups.edu.appdis.g1.dao.RolDao;
+import ec.ups.edu.appdis.g1.modelo.Rol;
+
+
+
+@Stateless
+public class RolON {
+
+	@Inject
+	private RolDao dao;
+
+	/*
+  	 * metodo que permite crear un rol  llamando al metodo crearRol de su clase dao
+  	 */
+	public void crearRol(Rol rol) {
+		dao.crearRol(rol);
+	}
+
+	/*
+  	 * metodo que permite listar los rol  llamando al metodo listaRol de su clase dao
+  	 */
+	public List<Rol> listaRol() {
+		return dao.listaRol();
+
+	}
+	
+	/*
+  	 * metodo que permite retornar un rol por medio de su id llamando al metodo buscar de su clase dao
+  	 */
+	public Rol buscar(int id) {
+		return dao.buscar(id);
+	}
+	
+	/*
+  	 * metodo que permite eliminar un rol  llamando al metodo delete de su clase dao
+  	 */
+	public void EliminarRol(Rol rol) {
+		dao.delete(rol);
+	}
+}
