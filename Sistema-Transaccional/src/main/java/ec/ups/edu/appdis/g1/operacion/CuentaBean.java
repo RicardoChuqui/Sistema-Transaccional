015ec03719f7168bc26c2ff7bdcd9150b37975cf
ricardo <rchuquio@est.ups.edu.ec>
2020-12-07 14:50:47 -0500

    socio

---
+++ Sistema-Transaccional/src/main/java/ec/ups/edu/appdis/g1/operacion/CuentaBean.java
@@ -0,0 +1,203 @@
+package ec.ups.edu.appdis.g1.operacion;
+
+
+
+
+
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import ec.ups.edu.appdis.g1.modelo.Cuenta;
+import ec.ups.edu.appdis.g1.negocio.GestionBancariaON;
+/**
+ *
+ * @author Ricardo Chuqui
+ */
+
+
+
+@ManagedBean
+@ViewScoped
+public class CuentaBean {
+    
+    @Inject
+    private GestionBancariaON on;
+    
+   private Cuenta newCuenta;
+   private List<Cuenta> listaCuenta;
+   
+  
+   
+
+   private Date  fecha=new Date();
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+   
+   
+
+    public Cuenta getNewCuenta() {
+        return newCuenta;
+    }
+
+    public void setNewCuenta(Cuenta newCuenta) {
+        this.newCuenta = newCuenta;
+    }
+
+    public List<Cuenta> getListaCuenta() {
+        return listaCuenta;
+    }
+
+    public void setListaCuenta(List<Cuenta> listaCuenta) {
+        this.listaCuenta = listaCuenta;
+    }
+
+
+
+   private String idCuenta;
+   
+      public String getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(String idCuenta) {
+        System.out.println("ID Cuenta: "+idCuenta);
+        this.idCuenta = idCuenta;
+        if(idCuenta!=null){
+            try {
+                newCuenta=on.buscarCuenta(idCuenta);
+               
+            } catch (Exception ex) {
+               System.out.println("Error setIDCuenta[BEAN]"+ex);
+            }
+        }
+        
+        
+    }
+    
+   
+    
+    public void init(){
+        try {
+            newCuenta=new Cuenta();
+            cargarCuentas();
+        } catch (Exception ex) {
+            System.out.println("Error init [CuentaBean]");
+        }
+    
+    }
+   
+    
+     /*    Cuenta             */
+    
+    
+  
+    
+   
+    
+    public String guardarDatosCuenta(){
+    
+        try {
+ 
+            on.guardarCuenta(newCuenta);
+            
+            System.out.println("Cuenta Guardada");
+
+        } catch (Exception ex) {
+            System.out.println("Guardar Cuenta..."+ex.getMessage());
+        }
+         return "listarCuentas";
+    }
+    
+
+    
+    
+    
+    public void cargarCuentas() throws Exception {
+        listaCuenta = on.listarCuentas();
+    }
+
+    public String eliminarCuenta(String idCuenta){
+    
+        try {
+            on.eliminarCuenta(idCuenta);
+        } catch (Exception ex) {
+            System.out.println("Error al Eliminar Cuenta [Bean]"+ex);
+        }
+        return null;
+    }
+    
+
+    
+    public String conpruebaCuentaExistente(String idCuenta){
+    
+        try {
+            
+            Cuenta busqueda=on.buscarCuenta(idCuenta);
+            
+            System.out.println("Encontrado"+busqueda.getIdCuenta());
+            System.out.println("Encontrado Pertenece"+busqueda.getSocio().getNombresSocio());
+           
+        } catch (Exception ex) {
+            System.out.println("Error al Buscar:"+ex.getMessage());
+                    
+        }
+         return null;
+    }
+    
+    
+    
+        public String redirigeCrearMovimiento(String idCuenta){
+         
+         System.out.println("Redirigir:"+idCuenta);
+        return "Movimiento?faces-redirect=true&idCuenta="+idCuenta;
+    }
+    
+       public String redirigeCrearRetiro(String idCuenta){
+         
+         System.out.println("Redirigir:"+idCuenta);
+        return "Retiro?faces-redirect=true&idCuenta="+idCuenta;
+    }
+    
+        
+        
+
+       /**
+        * Numero de Cuenta
+        */
+       
+       public static long numbGen() {
+       while (true) {
+           long numb = (long)(Math.random() * 100000000 * 1000000); // had to use this as int's are to small for a 13 digit number.
+           if (String.valueOf(numb).length() == 12)
+               return numb;
+       }
+   }
+       
+     String aleatorio=String.valueOf(numbGen());
+       
+       public String numero(){
+       return aleatorio;
+       }
+
+       public String getAleatorio() {
+           return aleatorio;
+       }
+
+       public void setAleatorio(String aleatorio) {
+           this.aleatorio = aleatorio;
+       }
+       
+    
+}
---
+++ Sistema-Transaccional/src/main/java/ec/ups/edu/appdis/g1/operacion/RegistroUsuarioBean.java
@@ -0,0 +1,147 @@
+package ec.ups.edu.appdis.g1.operacion;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import ec.ups.edu.appdis.g1.modelo.Rol;
+import ec.ups.edu.appdis.g1.modelo.UsuarioAdministrativo;
+import ec.ups.edu.appdis.g1.negocio.RolON;
+import ec.ups.edu.appdis.g1.negocio.UsuarioAdminON;
+
+
+
+@ManagedBean
+@ViewScoped
+public class RegistroUsuarioBean {
+
+	@Inject
+	private RolON ron;
+
+	@Inject
+	private UsuarioAdminON onadmi;
+
+	private UsuarioAdministrativo uadmi;
+
+	private Rol rol;
+
+	private List<Rol> rols;
+
+	private int id;
+	
+	private static int iduser;
+
+	private List<UsuarioAdministrativo> listaUAdmi;
+
+	public List<UsuarioAdministrativo> getListaUAdmi() {
+		return listaUAdmi;
+	}
+
+	public void setListaUAdmi(List<UsuarioAdministrativo> listaUAdmi) {
+		this.listaUAdmi = listaUAdmi;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public UsuarioAdministrativo getUadmi() {
+		return uadmi;
+	}
+
+	public void setUadmi(UsuarioAdministrativo uadmi) {
+		this.uadmi = uadmi;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public List<Rol> getRols() {
+		return rols;
+	}
+
+	public void setRols(List<Rol> rols) {
+		this.rols = rols;
+	}
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@PostConstruct
+	public void init() {
+		rols = ron.listaRol();
+
+		uadmi = new UsuarioAdministrativo();
+		recuperarUAdmi();
+	}
+
+	public String guardarDatos() {
+		try {
+			rol = new Rol();
+
+			uadmi.setRol(ron.buscar(id));
+			onadmi.crearUsuarioAdmi(uadmi);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "listar";
+	}
+
+	public void recuperarUAdmi() {
+		try {
+			listaUAdmi = onadmi.listarUAdmi();
+			
+			List<UsuarioAdministrativo>listaua = onadmi.listarUAdmi();
+			for (UsuarioAdministrativo ua : listaua) {
+				System.out.println("Nombre:"+ua.getNombre() );
+				System.out.println("Apellido:"+ua.getApellido() );
+				System.out.println("Usuario:"+ua.getUsuario());
+				System.out.println("Clave:"+ua.getContrasena() );
+				System.out.println("Rol:"+ua.getRol().getNombre() );
+				
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error al Listar" + e.getMessage());
+		}
+
+	}
+	public String eliminarUadmi(int idu) throws Exception {
+
+		try {
+			onadmi.eliminarUAdmi(idu);
+			System.out.println("Administrador Eliminado..." +idu);
+
+		} catch (Exception ex) {
+			throw new Exception("Error al eliminar" + ex);
+
+		}
+		return null;
+	}
+
+	
+
+}
